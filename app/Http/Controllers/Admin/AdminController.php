260cfce1df5033105fae77951de9974f28ab807b
cartory <xXcartoryXx@gmail.com>
2019-11-24 23:04:04 -0500

    ,

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -6,12 +6,7 @@ use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('admin');
-    }
-
+    
     public function index()
     {
         return view('admin.dashboard');
